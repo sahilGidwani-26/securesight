5c51c2f446b0427c520abb160a8fef2523475eb2
sahilGidwani-26 <gidwanisahil80@gmail.com>
2025-07-25 14:00:56 +0530

    Add files via upload

---
+++ securesight-frontend/src/app/layout.js
@@ -0,0 +1,12 @@
+// app/layout.js
+import './globals.css';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-100 text-gray-900">
+        {children}
+      </body>
+    </html>
+  );
+}
